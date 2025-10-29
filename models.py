9bf38712683812d015f7d95dbdec7b21fc2d4acd
CSP3103 <christian.solano1331@gmail.com>
2025-10-28 22:17:44 -0500

    Modelos Productos creado

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic import PositiveFloat, Field as PydanticField
 
+#Modelo Categoria
+
 class CategoryBase(SQLModel):
     # Lógica de Negocio: Nombre de categoría único
     name: str = Field(unique=True, index=True, min_length=3, max_length=50)
@@ -11,3 +13,24 @@ class CategoryBase(SQLModel):
 class Category(CategoryBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     products: List["Product"] = Relationship(back_populates="category")
+
+#Modelo Productos
+
+class ProductBase(SQLModel):
+    name: str = Field(index=True, min_length=3, max_length=100)
+    
+    # Validación Pydantic: precio debe ser positivo.
+    price: PositiveFloat = PydanticField(..., description="Product price (must be positive).")
+    
+    # Lógica de Negocio: Stock no negativo
+    stock: int = Field(default=0, ge=0) 
+    
+    description: Optional[str] = None
+    is_active: bool = Field(default=True)
+    
+    # Lógica de Negocio: Todos los productos deben tener categoría
+    category_id: int = Field(foreign_key="category.id", index=True)
+
+class Product(ProductBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    category: "Category" = Relationship(back_populates="category")
