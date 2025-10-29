9701e30c1c7a2f7bfa874efcafa00a0a0c7a6de1
CSP3103 <christian.solano1331@gmail.com>
2025-10-28 22:11:30 -0500

    Modelos Categorias creado

--- models.py
+++ models.py
@@ -2,3 +2,12 @@ from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic import PositiveFloat, Field as PydanticField
 
+class CategoryBase(SQLModel):
+    # Lógica de Negocio: Nombre de categoría único
+    name: str = Field(unique=True, index=True, min_length=3, max_length=50)
+    description: Optional[str] = None
+    is_active: bool = Field(default=True)
+
+class Category(CategoryBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    products: List["Product"] = Relationship(back_populates="category")
