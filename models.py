347d90ef2b3d381cc6bb9b00cd8f33726e87f0d9
CSP3103 <christian.solano1331@gmail.com>
2025-10-28 22:23:23 -0500

    Cambios en variables en models ya creados

--- models.py
+++ models.py
@@ -1,36 +1,28 @@
 from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
-from pydantic import PositiveFloat, Field as PydanticField
+from pydantic import PositiveFloat, Field as CampoPydantic
 
 #Modelo Categoria
 
-class CategoryBase(SQLModel):
-    # Lógica de Negocio: Nombre de categoría único
-    name: str = Field(unique=True, index=True, min_length=3, max_length=50)
-    description: Optional[str] = None
-    is_active: bool = Field(default=True)
+class CategoriaBase(SQLModel):
+    nombre: str = Field(unique=True, index=True, min_length=3, max_length=50)
+    descripcion: Optional[str] = None
+    esta_activo: bool = Field(default=True)
 
-class Category(CategoryBase, table=True):
+class Categoria(CategoriaBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    products: List["Product"] = Relationship(back_populates="category")
+    productos: List["Producto"] = Relationship(back_populates="categoria")
 
 #Modelo Productos
 
-class ProductBase(SQLModel):
-    name: str = Field(index=True, min_length=3, max_length=100)
-    
-    # Validación Pydantic: precio debe ser positivo.
-    price: PositiveFloat = PydanticField(..., description="Product price (must be positive).")
-    
-    # Lógica de Negocio: Stock no negativo
+class ProductoBase(SQLModel):
+    nombre: str = Field(index=True, min_length=3, max_length=100)
+    precio: PositiveFloat = CampoPydantic(..., description="Precio del producto (debe ser positivo).")
     stock: int = Field(default=0, ge=0) 
-    
-    description: Optional[str] = None
-    is_active: bool = Field(default=True)
-    
-    # Lógica de Negocio: Todos los productos deben tener categoría
-    category_id: int = Field(foreign_key="category.id", index=True)
+    descripcion: Optional[str] = None
+    esta_activo: bool = Field(default=True)
+    id_categoria: int = Field(foreign_key="categoria.id", index=True)
 
-class Product(ProductBase, table=True):
+class Producto(ProductoBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    category: "Category" = Relationship(back_populates="category")
+    categoria: "Categoria" = Relationship(back_populates="productos")
