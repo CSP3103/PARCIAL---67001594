2c2210a4fdb488cf5d1b20bc9012e57646b2f4cc
CSP3103 <christian.solano1331@gmail.com>
2025-10-28 23:19:46 -0500

    Post creado en productos

--- productos.py
+++ productos.py
@@ -6,3 +6,17 @@ from db import get_session
 from models import (Producto, ProductoBase, ProductoActualizacion, ProductoLecturaConCategoria, ProductoLectura,Categoria)
 
 router_productos = APIRouter(prefix="/products",tags=["Gestión de Productos"])
+
+@router_productos.post("/", response_model=ProductoLectura, status_code=status.HTTP_201_CREATED)
+async def crear_producto(*, session: Session = Depends(get_session), product_in: ProductoBase):
+    """Criterio: POST. Crea producto (201). Lógica: Valida categoría requerida (400)[cite: 50]."""
+    # Usamos product_in.id_categoria y la clase Categoria
+    category = session.get(Categoria, product_in.id_categoria)
+    if not category:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La categoría especificada no existe (400).")
+        
+    product = Producto.model_validate(product_in)
+    session.add(product)
+    session.commit()
+    session.refresh(product)
+    return product
